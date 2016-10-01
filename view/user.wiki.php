bf2b394b24467d35b22a624f786c1b4c9fd6a6f1
Jérémy Touati <jeremy.touati@gmail.com>
2016-10-01 19:52:12 +0200

    Added "Closest Achievements" section

--- view/user.wiki.php
+++ view/user.wiki.php
@@ -6,5 +6,12 @@
 Level <?php echo $user_profession->level ?> (<?php echo $user_profession->kudos ?> Kudos)
 <?php endforeach ?>
 <?php endif ?>
+
 <h2>Unlocked achievements</h2>
 <AchievementList user="<?php echo $user->getName() ?>" />
+
+<h2>Closest Achievements</h2>
+<?php foreach (PleesherExtension::getClosestAchievements($user->getId(), 3) as $goal): ?>
+<Goal code="<?php echo $goal->code ?>" perspective="<?php echo $user->getName() ?>" />
+<br>
+<?php endforeach ?>

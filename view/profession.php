7b092379d550f88721c89481d0dd8d9a92cc0af1
Jérémy Touati <jeremy.touati@gmail.com>
2016-10-01 17:45:06 +0200

    Fixed text key

--- view/profession.php
+++ view/profession.php
@@ -2,7 +2,7 @@
 	<h1><?php echo htmlspecialchars($profession->title) ?></h1>
 	<?php echo htmlspecialchars($profession->description) ?>
 
-	<h2><?php echo $h->text('profession.levels.title') ?></h2>
+	<h2><?php echo $h->text('liquigoals.profession.levels.title') ?></h2>
 	<ul>
 	<?php foreach ($profession->levels_kudos as $level => $kudos): ?>
 	<li>Level <?php echo $level + 1 ?> : <?php echo $kudos ?> Kudos</li>

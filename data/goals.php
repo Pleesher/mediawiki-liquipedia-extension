dcff3bd9a3ae46c8f4d3c06083a222b19dfe5f62
Jérémy Touati <jeremy.touati@gmail.com>
2016-12-11 19:39:40 +0100

    Goals definition update

--- data/goals.php
+++ data/goals.php
@@ -1,5 +1,68 @@
 <?php
 return [
+	'contributor_bronze' => [
+		'checker' => function($goal, $user_id, array $context) {
+			return $context['query_helper']->getUserEditCount($user_id) >= 1;
+		}
+	],
+	'contributor_silver' => [
+		'checker' => function($goal, $user_id, array $context) {
+			return [$context['query_helper']->getUserEditCount($user_id), 50];
+		}
+	],
+	'contributor_gold' => [
+		'checker' => function($goal, $user_id, array $context) {
+			return [$context['query_helper']->getUserEditCount($user_id), 2500];
+		}
+	],
+	'contributor_diamond' => [
+		'checker' => function($goal, $user_id, array $context) {
+			return [$context['query_helper']->getUserEditCount($user_id), 100000];
+		}
+	],
+
+	'player_contributor_bronze' => [
+		'checker' => function($goal, $user_id, array $context) {
+			return $context['query_helper']->getUserEditCount($user_id, ['category_title' => 'Players']) >= 1;
+		}
+	],
+	'player_contributor_silver' => [
+		'checker' => function($goal, $user_id, array $context) {
+			return [$context['query_helper']->getUserEditCount($user_id, ['category_title' => 'Players']), 50];
+		}
+	],
+	'player_contributor_gold' => [
+		'checker' => function($goal, $user_id, array $context) {
+			return [$context['query_helper']->getUserEditCount($user_id, ['category_title' => 'Players']), 2500];
+		}
+	],
+	'player_contributor_diamond' => [
+		'checker' => function($goal, $user_id, array $context) {
+			return [$context['query_helper']->getUserEditCount($user_id, ['category_title' => 'Players']), 100000];
+		}
+	],
+
+	'team_contributor_bronze' => [
+		'checker' => function($goal, $user_id, array $context) {
+			return $context['query_helper']->getUserEditCount($user_id, ['category_title' => 'Teams']) >= 1;
+		}
+	],
+	'team_contributor_silver' => [
+		'checker' => function($goal, $user_id, array $context) {
+			return [$context['query_helper']->getUserEditCount($user_id, ['category_title' => 'Teams']), 50];
+		}
+	],
+	'team_contributor_gold' => [
+		'checker' => function($goal, $user_id, array $context) {
+			return [$context['query_helper']->getUserEditCount($user_id, ['category_title' => 'Teams']), 2500];
+		}
+	],
+	'team_contributor_diamond' => [
+		'checker' => function($goal, $user_id, array $context) {
+			return [$context['query_helper']->getUserEditCount($user_id, ['category_title' => 'Teams']), 100000];
+		}
+	],
+
 	'massive_edit' => [
 		'checker' => function($goal, $user_id, array $context) {
 			return [$context['query_helper']->getMaxUserEditLength($user_id), 100000];
@@ -12,30 +75,9 @@ return [
 		},
 		'professions' => ['historian', 'reporter']
 	],
-	'madman' => [
-		'checker' => function($goal, $user_id, array $context) {
-			return [$context['query_helper']->getUserEditCount($user_id), 20000];
-		},
-		'professions' => ['historian']
-	],
-	'first_edit' => [
-		'checker' => function($goal, $user_id, array $context) {
-			return $context['query_helper']->getUserEditCount($user_id) > 0;
-		}
-	],
-	'contributor' => [
-		'checker' => function($goal, $user_id, array $context) {
-			return [$context['query_helper']->getUserEditCount($user_id), 10];
-		}
-	],
 	'necromancer' => [
 		'checker' => function($goal, $user_id, array $context) {
 			return [$context['query_helper']->getMaxBumpDays($user_id), 364];
 		}
 	],
-	'beginner_biographer' => [
-		'checker' => function($goal, $user_id, array $context) {
-			return [$context['query_helper']->getUserEditCount($user_id, ['category_title' => 'Players']), 10];
-		}
-	]
 ];

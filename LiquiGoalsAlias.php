134dd9b3fb9358602a0628f743c939abe55e8b8f
FO-nTTaX <fo.nttax@yahoo.de>
2017-04-06 13:48:02 +0200

    add aliases for pages

---
+++ LiquiGoalsAlias.php
@@ -0,0 +1,12 @@
+<?php
+
+$specialPageAliases = array();
+
+$specialPageAliases['en'] = array(
+	'Achievements' => array( 'Achievements' ),
+	'UserRanking' => array( 'UserRanking' ),
+	'AchievementsAdmin' => array( 'AchievementsAdmin' ),
+	'AchievementDetails' => array( 'AchievementDetails' ),
+	'AchievementsError' => array( 'AchievementsError' ),
+	'Test' => array( 'Test' ),//remove this if you remove the LiquiGoals_TestPage special page
+);

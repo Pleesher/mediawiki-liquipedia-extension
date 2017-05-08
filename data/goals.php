5151009d13ad17eaa56fa93c74258b1ee69ac563
Jérémy Touati <jeremy.touati@gmail.com>
2017-05-08 21:30:21 +0200

    Category is now fetched from Pleesher's data (API)

--- data/goals.php
+++ data/goals.php
@@ -7,225 +7,189 @@ return [
 	'content_creator_bronze' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return $context['query_helper']->getUserPageCreationCount(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]) >= 1;
-		},
-		'category' => 'general'
+		}
 	],
 	'content_creator_silver' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserPageCreationCount(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 10];
-		},
-		'category' => 'general'
+		}
 	],
 	'content_creator_gold' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserPageCreationCount(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 100];
-		},
-		'category' => 'general'
+		}
 	],
 	'content_creator_diamond' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserPageCreationCount(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 1000];
-		},
-		'category' => 'general'
+		}
 	],
 
 	'contributor_bronze' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return $context['query_helper']->getUserEditCount(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]) >= 1;
-		},
-		'category' => 'general'
+		}
 	],
 	'contributor_silver' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 50];
-		},
-		'category' => 'general'
+		}
 	],
 	'contributor_gold' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 2500];
-		},
-		'category' => 'general'
+		}
 	],
 	'contributor_diamond' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 100000];
-		},
-		'category' => 'general'
+		}
 	],
 
 	'player_contributor_bronze' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return $context['query_helper']->getUserEditCount(User::idFromName($user_name), ['category_title' => 'Players', '_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]) >= 1;
-		},
-		'category' => 'players'
+		}
 	],
 	'player_contributor_silver' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name), ['category_title' => 'Players', '_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 50];
-		},
-		'category' => 'players'
+		}
 	],
 	'player_contributor_gold' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name), ['category_title' => 'Players', '_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 2500];
-		},
-		'category' => 'players'
+		}
 	],
 	'player_contributor_diamond' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name), ['category_title' => 'Players', '_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 100000];
-		},
-		'category' => 'players'
+		}
 	],
 
 	'team_contributor_bronze' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return $context['query_helper']->getUserEditCount(User::idFromName($user_name), ['category_title' => 'Teams', '_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]) >= 1;
-		},
-		'category' => 'teams'
+		}
 	],
 	'team_contributor_silver' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name), ['category_title' => 'Teams', '_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 50];
-		},
-		'category' => 'teams'
+		}
 	],
 	'team_contributor_gold' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name), ['category_title' => 'Teams', '_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 2500];
-		},
-		'category' => 'teams'
+		}
 	],
 	'team_contributor_diamond' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name), ['category_title' => 'Teams', '_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 100000];
-		},
-		'category' => 'teams'
+		}
 	],
 
 	'transfer_contributor_bronze' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return $context['query_helper']->getUserEditCount(User::idFromName($user_name), ['page_title_regex' => '([[:<:]]|_|^)[tT]ransfers?([[:>:]]|_|$)', '_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]) >= 1;
-		},
-		'category' => 'transfers'
+		}
 	],
 	'transfer_contributor_silver' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name),  ['page_title_regex' => '([[:<:]]|_|^)[tT]ransfers?([[:>:]]|_|$)', '_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 50];
-		},
-		'category' => 'transfers'
+		}
 	],
 	'transfer_contributor_gold' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name),  ['page_title_regex' => '([[:<:]]|_|^)[tT]ransfers?([[:>:]]|_|$)', '_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 2500];
-		},
-		'category' => 'transfers'
+		}
 	],
 	'transfer_contributor_diamond' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name),  ['page_title_regex' => '([[:<:]]|_|^)[tT]ransfers?([[:>:]]|_|$)', '_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 100000];
-		},
-		'category' => 'transfers'
+		}
 	],
 
 	'template_contributor_bronze' => [
 		'checker' => function($goal, $user_name, array $context) {
 			return $context['query_helper']->getUserEditCount(User::idFromName($user_name), ['namespace' => NS_TEMPLATE]) >= 1;
-		},
-		'category' => 'improving_liquipedia'
+		}
 	],
 	'template_contributor_silver' => [
 		'checker' => function($goal, $user_name, array $context) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name), ['namespace' => NS_TEMPLATE]), 50];
-		},
-		'category' => 'improving_liquipedia'
+		}
 	],
 	'template_contributor_gold' => [
 		'checker' => function($goal, $user_name, array $context) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name), ['namespace' => NS_TEMPLATE]), 2500];
-		},
-		'category' => 'improving_liquipedia'
+		}
 	],
 	'template_contributor_diamond' => [
 		'checker' => function($goal, $user_name, array $context) {
 			return [$context['query_helper']->getUserEditCount(User::idFromName($user_name), ['namespace' => NS_TEMPLATE]), 100000];
-		},
-		'category' => 'improving_liquipedia'
+		}
 	],
 
 	'template_creator_bronze' => [
 		'checker' => function($goal, $user_name, array $context) use($_team_templates_page_title_regex) {
 			return $context['query_helper']->getUserPageCreationCount(User::idFromName($user_name), ['namespace' => NS_TEMPLATE, 'page_title_negative_regex' => $_team_templates_page_title_regex]) >= 1;
-		},
-		'category' => 'improving_liquipedia'
+		}
 	],
 	'template_creator_silver' => [
 		'checker' => function($goal, $user_name, array $context) use($_team_templates_page_title_regex) {
 			return [$context['query_helper']->getUserPageCreationCount(User::idFromName($user_name), ['namespace' => NS_TEMPLATE, 'page_title_negative_regex' => $_team_templates_page_title_regex]), 10];
-		},
-		'category' => 'improving_liquipedia'
+		}
 	],
 	'template_creator_gold' => [
 		'checker' => function($goal, $user_name, array $context) use($_team_templates_page_title_regex) {
 			return [$context['query_helper']->getUserPageCreationCount(User::idFromName($user_name), ['namespace' => NS_TEMPLATE, 'page_title_negative_regex' => $_team_templates_page_title_regex]), 100];
-		},
-		'category' => 'improving_liquipedia'
+		}
 	],
 	'template_creator_diamond' => [
 		'checker' => function($goal, $user_name, array $context) use($_team_templates_page_title_regex) {
 			return [$context['query_helper']->getUserPageCreationCount(User::idFromName($user_name), ['namespace' => NS_TEMPLATE, 'page_title_negative_regex' => $_team_templates_page_title_regex]), 1000];
-		},
-		'category' => 'improving_liquipedia'
+		}
 	],
 
 	'edit_streak_bronze' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserMaxEditStreak(User::idFromName($user_name)), 2];
-		},
-		'category' => 'general'
+		}
 	],
 	'edit_streak_silver' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserMaxEditStreak(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 7];
-		},
-		'category' => 'general'
+		}
 	],
 	'edit_streak_gold' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserMaxEditStreak(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 30];
-		},
-		'category' => 'general'
+		}
 	],
 	'edit_streak_diamond' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserMaxEditStreak(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 365];
-		},
-		'category' => 'general'
+		}
 	],
 
 	'necromancer_bronze' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserMaxBumpDays(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 364];
-		},
-		'category' => 'unconventional'
+		}
 	],
 	'necromancer_silver' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserMaxBumpDays(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 364 * 2];
-		},
-		'category' => 'unconventional'
+		}
 	],
 	'necromancer_gold' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserMaxBumpDays(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 364 * 5];
-		},
-		'category' => 'unconventional'
+		}
 	],
 	'necromancer_diamond' => [
 		'checker' => function($goal, $user_name, array $context) use($_main_namespaces_filter, $_main_namespaces_filter_params) {
 			return [$context['query_helper']->getUserMaxBumpDays(User::idFromName($user_name), ['_where' => $_main_namespaces_filter, '_where_params' => $_main_namespaces_filter_params]), 364 * 10];
-		},
-		'category' => 'unconventional'
+		}
 	],
 ];

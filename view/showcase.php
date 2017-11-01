150da84f14d351332554f9e3567bcec64260fbf7
FO-nTTaX <fo.nttax@yahoo.de>
2017-11-01 13:14:39 +0100

    add classes for images so we can out custom ones

--- view/showcase.php
+++ view/showcase.php
@@ -1,6 +1,6 @@
 <div class="pleesher-showcase">
 	<?php foreach ($goals as $showcased_goal): ?>
-	<div class="pleesher-showcase-item pleesher-item-image-<?php echo $showcased_goal->id; ?>" title="<?php echo $showcased_goal->title ?>" data-toggle="popover-hover" data-placement="top" data-content="<?php echo $showcased_goal->short_description ?>">
+	<div class="pleesher-showcase-item pleesher-item-image-<?php echo str_replace(' ', '-', trim(preg_filter('/[^a-z ]/', '', strtolower($showcased_goal->title)))) ?>" title="<?php echo $showcased_goal->title ?>" data-toggle="popover-hover" data-placement="top" data-content="<?php echo $showcased_goal->short_description ?>">
 		<div class="pleesher-showcase-item-icon"><?php if ($removable && !PleesherExtension::isDisabled()): ?>
 		<a class="pleesher-showcase-item-icon-remove" data-redirect="self" href="<?php echo $h->actionUrl('pleesher.showcase_achievement', ['goal_id' => $showcased_goal->id, 'remove' => 1]) ?>"><i class="fa fa-times" aria-hidden="true"></i>
 		</a><?php endif
